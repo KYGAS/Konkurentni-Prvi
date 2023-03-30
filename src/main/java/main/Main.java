d9289c076773e305d7cd87c361c616e7dc949847
KYGAS <acazvezdas@gmail.com>
2023-03-30 03:43:55 +0200

    Add directory crawler and command handler to main

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,5 +1,7 @@
 package main;
 
+import DirCrawler.DirectoryCrawler;
+import cli.CommandHandler;
 import config.Config;
 
 import java.io.IOException;
@@ -18,12 +20,62 @@ public class Main {
             System.out.println("Config doesn't exist.");
         }
         if(config == null){
-            System.exit(0);
+            exitApp("Config loader error!");
         }
         else {
             System.out.println("App config loaded...");
         }
         // ---------------------------------- Loaded Config
 
+        // ---------------------------------- Loading Directory Crawler
+        Thread directoryCrawlerThread;
+        DirectoryCrawler directoryCrawler;
+        System.out.println("Loading directory crawler...");
+        try{
+            directoryCrawler = new DirectoryCrawler();
+            directoryCrawlerThread = new Thread(directoryCrawler);
+            directoryCrawlerThread.start();
+        }
+        catch (Exception ignored){
+            directoryCrawler = null;
+        }
+        if(directoryCrawler == null){
+            exitApp("Directory crawler error!");
+        }
+        else {
+            System.out.println("Directory crawler loaded...");
+        }
+        // ---------------------------------- Loaded Directory Crawler
+
+
+
+
+
+
+        // ---------------------------------- Creating a command handler
+        CommandHandler commandHandler;
+        System.out.println("Loading directory crawler...");
+        try{
+            commandHandler = new CommandHandler();
+        }
+        catch (Exception ignored){
+            commandHandler = null;
+        }
+        if(commandHandler == null){
+            exitApp("Command handler couldn't be created!");
+        }
+        else {
+            System.out.println("Command handler created!");
+            commandHandler.addRunningObject(directoryCrawler);
+            commandHandler.beginInputRead();
+        }
+        // ---------------------------------- Created a command handler
+        exitApp("Application closed!");
+    }
+
+
+    private static void exitApp(String message){
+        System.out.println(message);
+        System.exit(0);
     }
 }
