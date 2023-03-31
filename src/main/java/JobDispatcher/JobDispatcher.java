55660410d8cde3e9230f29e1a0e54536930606b8
KYGAS <acazvezdas@gmail.com>
2023-03-31 19:29:12 +0200

    Implement JobDispatcher class : - jobBlockingQueue into switch by class name

--- src/main/java/JobDispatcher/JobDispatcher.java
+++ src/main/java/JobDispatcher/JobDispatcher.java
@@ -1,4 +1,47 @@
 package JobDispatcher;
 
-public class JobDispatcher {
+import JobQueue.*;
+import utils.Notifier;
+
+import static JobQueue.JobQueue.jobBlockingQueue;
+
+public class JobDispatcher implements Runnable {
+
+    public volatile boolean isRunning;
+
+    @Override
+    public void run() {
+        isRunning = true;
+        while (isRunning) {
+            try {
+                // Wait for a new job to appear in the queue
+                Job job = jobBlockingQueue.take();
+
+                switch (job.getClass().getSimpleName()) {
+                    case "FileJob" -> {
+                        System.out.println("New File Job!");
+                    }
+                    case "WebJob" -> {
+                        System.out.println("New Web Job!");
+                    }
+                    default -> {
+                        System.out.println("Invalid Job");
+                    }
+                }
+            }
+            catch (InterruptedException e) {
+                System.out.println("JobDispatcherThread was interrupted: " + e.getMessage());
+                return;
+            }
+        }
+        Notifier.notifyObjectStopped(this);
+    }
+
+    public void stop(){
+        isRunning = false;
+    }
+
+    public JobDispatcher() {
+
+    }
 }
