863e9ad9e7af249584860f3a4fc5f3036aaea62b
KYGAS <acazvezdas@gmail.com>
2023-04-01 06:04:53 +0200

    Create a file job constructor with an argument

--- src/main/java/JobQueue/FileJob.java
+++ src/main/java/JobQueue/FileJob.java
@@ -2,6 +2,11 @@ package JobQueue;
 
 public class FileJob implements Job{
 
+    public String path;
+
+    public FileJob(String path) {
+        this.path = path;
+    }
 
     public FileJob() {
     }
