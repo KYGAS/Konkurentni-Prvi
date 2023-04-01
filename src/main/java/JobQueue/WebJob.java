25060fcad98bdafbf549de1e3bfb151ee166e284
KYGAS <acazvezdas@gmail.com>
2023-04-01 06:05:06 +0200

    Create a web job constructor with arguments

--- src/main/java/JobQueue/WebJob.java
+++ src/main/java/JobQueue/WebJob.java
@@ -2,6 +2,13 @@ package JobQueue;
 
 public class WebJob implements Job{
 
+    public String path;
+    public Integer hops;
+
+    public WebJob(String path, Integer hops) {
+        this.path = path;
+        this.hops = hops;
+    }
 
     public WebJob() {
     }
