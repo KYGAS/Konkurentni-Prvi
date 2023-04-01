73c1f3848a730f5a01141c101f644fce8ba040ca
KYGAS <acazvezdas@gmail.com>
2023-04-01 06:02:51 +0200

    Add new util function : - Inform if thread was *interrupted*

--- src/main/java/utils/Notifier.java
+++ src/main/java/utils/Notifier.java
@@ -4,4 +4,8 @@ public class Notifier {
     public static void notifyObjectStopped(Object object){
         System.out.println("Stopped " + object.getClass().getName() );
     }
+
+    public static void notifyForceStop(Object object){
+        System.out.println("Thread < " + object.getClass().getName() + " > has forcefully stopped!");
+    }
 }
