d601158b2a9e330b160de0ca4bc393f1496f8082
KYGAS <acazvezdas@gmail.com>
2023-04-01 06:06:11 +0200

    Implement WEB pooling/scanner ( initial ) : - implement pooling of jobs - implement closing of thread pool ( awaiting closing job implementation ) - implement initial scanning ( only first keyword ) ( awaiting hops )

--- src/main/java/Jobs/Pools/Web.java
+++ src/main/java/Jobs/Pools/Web.java
@@ -1,5 +1,40 @@
 package Jobs.Pools;
 
+import JobQueue.WebJob;
+import utils.Notifier;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class Web {
-    // TODO : Contain Web Job POOLING logic here
+
+    public static ExecutorService executorService;
+    public Web() {
+        executorService = Executors.newCachedThreadPool();
+    }
+
+    public static void queueWebJob(WebJob webJob){
+        executorService.submit(
+                new Jobs.Scanners.Web(
+                        webJob.path,
+                        webJob.hops
+                )
+        );
+    }
+
+    public static void closeWebJobs(){
+        try {
+            if (executorService.awaitTermination(Integer.MAX_VALUE, TimeUnit.SECONDS)) {
+                Notifier.notifyObjectStopped(new Web());
+            } else {
+                Notifier.notifyForceStop(new Web());
+            }
+        }
+        catch (Exception ignored){
+            Notifier.notifyObjectStopped(new Web());
+        }
+    }
+
+
 }
--- src/main/java/Jobs/Scanners/Web.java
+++ src/main/java/Jobs/Scanners/Web.java
@@ -1,5 +1,48 @@
 package Jobs.Scanners;
 
-public class Web {
+import config.Config;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
+public class Web implements Runnable{
     // TODO : Contain Web Job SCAN logic here
+    String path;
+    Integer hops;
+
+    public Web(String path, Integer hops) {
+        this.path = path;
+        this.hops = hops;
+    }
+
+    @Override
+    public void run() {
+        String keyword = new Config().getKeywords()[0];
+
+        try {
+
+            Document doc = Jsoup.connect(path).get();
+            String text = doc.body().text();
+            int count = 0;
+            int index = 0;
+
+            while (index != -1) {
+                index = text.indexOf(keyword, index);
+                if (index != -1) {
+                    count++;
+                    index += keyword.length();
+                }
+            }
+
+            System.out.println("The keyword \"" + keyword + "\" appears " + count + " times on the page.");
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+
+        System.out.println("Count : " + keyword);
+
+    }
 }
