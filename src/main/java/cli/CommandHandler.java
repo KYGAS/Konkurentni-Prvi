99342aaf571804fd39cebb60897d9f641cd3f3fe
KYGAS <acazvezdas@gmail.com>
2023-04-03 04:53:01 +0200

    Set main web job parent as null

--- src/main/java/cli/CommandHandler.java
+++ src/main/java/cli/CommandHandler.java
@@ -35,7 +35,7 @@ public class CommandHandler {
                 if (parts.length >= 2) {
                     String path = parseStringParameter(command.substring(cmd.length() + 1));
                     System.out.println("Website added : " + path);
-                    JobQueue.addJob(new WebJob(path, new Config().getHopCount()));
+                    JobQueue.addJob(new WebJob(path, new Config().getHopCount(), null));
                 }
                 else {
                     System.out.println("Invalid parameter length for aw command");
